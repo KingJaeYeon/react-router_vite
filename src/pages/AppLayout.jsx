d69cc2d6daee5001041fe2d890ccac8d39e148dc
pjy <wodus331@gmail.com>
2023-09-23 15:25:29 +0900

    start

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav.jsx";
+
+function AppLayout() {
+    return (
+        <div>
+            <AppNav/>
+            <p>App</p>
+        </div>
+    );
+}
+
+export default AppLayout;
