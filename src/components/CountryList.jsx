4ecbb7b40e2e39767777e08181365036c6b51b00
pjy <wodus331@gmail.com>
2023-09-23 21:01:46 +0900

    fix

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -4,24 +4,25 @@ import Spinner from "./Spinner.jsx";
 import Message from "./Message.jsx";
 import CountryItem from "./CountryItem.jsx";
 
-function CountryList({cities,isLoading}) {
+function CountryList({cities, isLoading}) {
     if (isLoading) return <Spinner/>;
 
-    if(!cities?.length)
+    if (!cities?.length)
         return <Message message={'Add your first city by clicking on a city on the map'}/>;
 
-    const countries = cities.reduce((arr,city) => {
-        if(!arr.map(el=>el.city).includes(city.county))
-            return [...arr , {
-
-            }]}
-        ,[]);
+    const countries = cities?.reduce((arr, city) => {
+            if (!arr.map(el => el?.country).includes(city.country))
+                return [...arr, {country: city.country, emoji: city.emoji, id: city.id}]
+            else return arr
+        }
+        , []);
 
     return (
         <ul className={styles.countryList}>
-            {countries?.map(county =>
-                <CountryItem key={county.id} county={county}/>)}
+            {countries?.map(country =>
+                <CountryItem key={country.id} country={country}/>)}
         </ul>
-    );}
+    );
+}
 
 export default CountryList;
