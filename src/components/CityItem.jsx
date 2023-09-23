408195bf8f0f13834eb8e228f2f53ac609ed9613
pjy <wodus331@gmail.com>
2023-09-23 22:45:01 +0900

    router commit

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,4 +1,5 @@
 import styles from './CityItem.module.css'
+import {Link} from "react-router-dom";
 
 const formatDate = (date) =>
     new Intl.DateTimeFormat('en', {
@@ -9,14 +10,17 @@ const formatDate = (date) =>
 
 
 function CityItem({city}) {
-    const {cityName, emoji, date} = city;
+    const {cityName, emoji, date, id, position} = city;
 
     return (
-        <li className={styles.cityItem}>
-            <span className={styles.emoji}>{emoji}</span>
-            <h3 className={styles.name}>{cityName}</h3>
-            <time className={styles.date}>{formatDate(date)}</time>
-            <button className={styles.deleteBtn}>&times;</button>
+        <li>
+            <Link className={styles.cityItem}
+                  to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+                <span className={styles.emoji}>{emoji}</span>
+                <h3 className={styles.name}>{cityName}</h3>
+                <time className={styles.date}>{formatDate(date)}</time>
+                <button className={styles.deleteBtn}>&times;</button>
+            </Link>
         </li>
     );
 }
