a6f3b8847f147beb063a768ca1ce5079ddc5d95b
pjy <wodus331@vanillacode.net>
2023-09-23 22:45:01 +0900

    router commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import HomePage from "./pages/HomePage.jsx";
@@ -8,6 +8,8 @@ import AppLayout from "./pages/AppLayout.jsx";
 import Login from "./pages/Login.jsx";
 import CityList from "./components/CityList.jsx";
 import CountriesList from "./components/CountryList.jsx";
+import City from "./components/City.jsx";
+import Form from "./components/Form.jsx";
 
 const BASE_URL = 'http://localhost:3600'
 
@@ -38,10 +40,11 @@ function App() {
                 <Route path='product' element={<Product/>}/>
                 <Route path='pricing' element={<Pricing/>}/>
                 <Route path='app' element={<AppLayout/>}>
-                    <Route index element={<CityList cities={cities} isLoading={isLoading}/>}/>
+                    <Route index element={<Navigate replace to='cities'/>}/>
                     <Route path='cities' element={<CityList cities={cities} isLoading={isLoading}/>}/>
+                    <Route path='cities/:id' element={<City />}/>
                     <Route path='countries' element={<CountriesList cities={cities} isLoading={isLoading}/>}/>
-                    <Route path='form' element={<div>Form</div>}/>
+                    <Route path='form' element={<Form />}/>
                 </Route>
                 <Route path='login' element={<Login/>}/>
                 <Route path='*' element={<PageNotFound/>}/>
