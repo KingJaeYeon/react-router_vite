f267c6200cfdaad19f9495c1a03ff2f6ae3bae37
pjy <wodus331@vanillacode.net>
2023-09-24 21:49:24 +0900

    end

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,16 +1,15 @@
 import styles from "./User.module.css";
-
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
+import {useAuth} from "../context/AuthContext.jsx";
+import {useNavigate} from "react-router-dom";
 
 function User() {
-  const user = FAKE_USER;
-
-  function handleClick() {}
+    const {user,logout} = useAuth();
+    const navigate = useNavigate();
+  function handleClick() {
+      logout();
+      navigate('/')
+  }
+ if (!user) return null;
 
   return (
     <div className={styles.user}>
